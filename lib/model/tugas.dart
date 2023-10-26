0ee5ba2df46be5216d5550818644586fdfd16514
anisameilia233@gmail.com <anisameilia233@gmail.com>
2023-10-26 19:22:43 +0700

    first commit

---
+++ lib/model/tugas.dart
@@ -0,0 +1,14 @@
+class Tugas {
+  int? id;
+  String? title;
+  String? description;
+  DateTime? deadline;
+  Tugas({this.id, this.title, this.description, this.deadline});
+  factory Tugas.fromJson(Map<String, dynamic> obj) {
+    return Tugas(
+        id: int.parse(obj['id']),
+        title: obj['title'].toString(),
+        description: obj['description'].toString(),
+        deadline: DateTime.parse(obj['deadline'].toString()));
+  }
+}
